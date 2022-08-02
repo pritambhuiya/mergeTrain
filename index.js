10886427fc41550f4af74392adef38f87cffa889
Pritam Bhuiya <99480050+pritambhuiya@users.noreply.github.com>
2022-08-02 15:14:52 +0530

    Applied try catch while reading file

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const fs = require('fs');
 const { startJourney } = require('./src/startJourney.js');
 
 const main = (inputFilePath) => {
-  const trains = fs.readFileSync(inputFilePath, 'utf8');
-  console.log(startJourney(trains));
+  try {
+    const trains = fs.readFileSync(inputFilePath, 'utf8');
+    console.log(startJourney(trains));
+
+  } catch (error) {
+    console.log('Can\'t read details from file', error.code);
+  }
 };
 
 main(...process.argv.slice(2));
